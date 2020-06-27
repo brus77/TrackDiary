de8aef48050bf62ad0e22f46a2508fb8f3b40be6
brus <carlo.bruscolotti@gmail.com>
2020-06-28 00:14:13 +0200

    Merge from origin

---
+++ TrackDiary.Model/DataAcquisition/InputDataSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using TrackDiary.Model.Common;
+using TrackDiary.Model.InputDevices;
+
+namespace TrackDiary.Model.DataAcquisition
+{
+    public class InputDataSet : IEntity<IntIdentityType>
+    {
+        public IntIdentityType Id { get; set; }
+
+        public DateTimeOffset StartTime { get; set; }
+        public DateTimeOffset? EndTime { get; set; }
+
+        public IEnumerable<InputSource> Sources { get; set; }
+        public IEnumerable<DataPoint<T>> DataPoints { get; set; }
+    }
+}
